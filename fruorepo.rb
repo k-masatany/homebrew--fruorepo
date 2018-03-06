bcd0bde653bd87268d848283359c3378c4e008e5
k-masatany <masatani@fusic.co.jp>
2018-03-06 10:40:39 +0900

    fix formula

--- fruorepo.rb
+++ fruorepo.rb
@@ -4,6 +4,7 @@ class Fruorepo < Formula
   homepage "https://github.com/k-masatany/fruorepo"
   url "https://github.com/k-masatany/fruorepo/releases/download/v0.1.0/fruorepo-releace-x86_64.tar.gz"
   sha256 "f4f97a4cd692a53c9ca17dfa1a41faf8fbc2ac643ebe7c4be247f4b56a0196fb"
+  head 'https://github.com/k-masatany/fruorepo.git', :branch => 'master'
 
   def install
     bin.install 'fruorepo'
