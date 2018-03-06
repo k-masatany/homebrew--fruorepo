d486b3859a135717c5b685cf3473bb1f08aa8fbf
k-masatany <masatani@fusic.co.jp>
2018-03-06 14:37:15 +0900

    fix sha256

--- fruorepo.rb
+++ fruorepo.rb
@@ -5,7 +5,7 @@ class Fruorepo < Formula
   desc "We enjoy repository."
   homepage "https://github.com/k-masatany/fruorepo"
   url "https://github.com/k-masatany/fruorepo/releases/download/#{HOMEBREW_FRUOREPO_VERSION}/fruorepo-releace-x86_64.tar.gz"
-  sha256 "f4f97a4cd692a53c9ca17dfa1a41faf8fbc2ac643ebe7c4be247f4b56a0196fb"
+  sha256 "97bbaf624537ec6363efab75784055b1fb18e46157b97aadbb9c57a6ba938a48"
   head 'https://github.com/k-masatany/fruorepo.git', :branch => 'master'
   version HOMEBREW_FRUOREPO_VERSION
 
