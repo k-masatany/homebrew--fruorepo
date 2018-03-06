793ffbf661672a0b9192a20c3bf9d4ba89b2d766
k-masatany <masatani@fusic.co.jp>
2018-03-06 14:09:57 +0900

    fix formula

--- fruorepo.rb
+++ fruorepo.rb
@@ -7,6 +7,7 @@ class Fruorepo < Formula
   url "https://github.com/k-masatany/fruorepo/releases/download/#{HOMEBREW_FRUOREPO_VERSION}/fruorepo-releace-x86_64.tar.gz"
   sha256 "f4f97a4cd692a53c9ca17dfa1a41faf8fbc2ac643ebe7c4be247f4b56a0196fb"
   head 'https://github.com/k-masatany/fruorepo.git', :branch => 'master'
+  version HOMEBREW_FRUOREPO_VERSION
 
   def install
     bin.install 'fruorepo'
