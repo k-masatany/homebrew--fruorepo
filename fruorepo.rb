1a9b5137dd7b9be4af0ffcd772548d5ef297fab9
k-masatany <masatani@fusic.co.jp>
2018-03-06 14:07:04 +0900

    v0.1.1

--- fruorepo.rb
+++ fruorepo.rb
@@ -1,8 +1,10 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+HOMEBREW_FRUOREPO_VERSION='v0.1.1'
+
 class Fruorepo < Formula
   desc "We enjoy repository."
   homepage "https://github.com/k-masatany/fruorepo"
-  url "https://github.com/k-masatany/fruorepo/releases/download/v0.1.0/fruorepo-releace-x86_64.tar.gz"
+  url "https://github.com/k-masatany/fruorepo/releases/download/#{HOMEBREW_FRUOREPO_VERSION}/fruorepo-releace-x86_64.tar.gz"
   sha256 "f4f97a4cd692a53c9ca17dfa1a41faf8fbc2ac643ebe7c4be247f4b56a0196fb"
   head 'https://github.com/k-masatany/fruorepo.git', :branch => 'master'
 
