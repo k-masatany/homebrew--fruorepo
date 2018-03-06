6ba52961d22074a2e0f88ff99033c8c67179eabf
k-masatany <masatani@fusic.co.jp>
2018-03-06 15:02:45 +0900

    fix formula

--- fruorepo.rb
+++ fruorepo.rb
@@ -1,13 +1,10 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-HOMEBREW_FRUOREPO_VERSION='v0.1.1'
-
 class Fruorepo < Formula
   desc "We enjoy repository."
   homepage "https://github.com/k-masatany/fruorepo"
-  url "https://github.com/k-masatany/fruorepo/releases/download/#{HOMEBREW_FRUOREPO_VERSION}/fruorepo-releace-x86_64.tar.gz"
+  url "https://github.com/k-masatany/fruorepo/releases/download/v0.1.1/fruorepo-releace-x86_64.tar.gz"
   sha256 "97bbaf624537ec6363efab75784055b1fb18e46157b97aadbb9c57a6ba938a48"
   head 'https://github.com/k-masatany/fruorepo.git', :branch => 'master'
-  version HOMEBREW_FRUOREPO_VERSION
 
   def install
     bin.install 'fruorepo'
