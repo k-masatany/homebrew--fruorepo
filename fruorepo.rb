df27bfc9902fc933e834445fef8dc56cc2b88a14
k-masatany <masatani@fusic.co.jp>
2018-03-08 22:16:28 +0900

    fix version

--- fruorepo.rb
+++ fruorepo.rb
@@ -2,7 +2,7 @@
 class Fruorepo < Formula
   desc "We enjoy repository."
   homepage "https://github.com/k-masatany/fruorepo"
-  url "https://github.com/k-masatany/fruorepo/releases/download/v0.1.1/fruorepo-release-x86_64.tar.gz"
+  url "https://github.com/k-masatany/fruorepo/releases/download/v0.2.0/fruorepo-release-x86_64.tar.gz"
   sha256 "f7132695d5a8b7addf87c6d88a8ed9d39416657f95b181da484598b0d98cf77a"
   head 'https://github.com/k-masatany/fruorepo.git', :branch => 'master'
 
