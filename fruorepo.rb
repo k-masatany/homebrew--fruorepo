106aa37a57fdcd07d1499d40e9214e9ee036655b
k-masatany <masatani@fusic.co.jp>
2018-04-11 13:18:46 +0900

    fix typo

--- fruorepo.rb
+++ fruorepo.rb
@@ -2,7 +2,7 @@
 class Fruorepo < Formula
   desc "We enjoy repository."
   homepage "https://github.com/k-masatany/fruorepo"
-  url "https://github.com/k-masatany/fruorepo/releases/download/v0.2.2/darwin_386_0.2.1.zip"
+  url "https://github.com/k-masatany/fruorepo/releases/download/v0.2.2/darwin_386_0.2.2.zip"
   sha256 "e3c21cb3e98e6378665362b503533c789578c039278309c56fb1b8a638d56f4d"
   head 'https://github.com/k-masatany/fruorepo.git', :branch => 'master'
 
