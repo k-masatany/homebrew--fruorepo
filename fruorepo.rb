0ce6cad27eb4008f6b49943c11118d23d1f389ef
k-masatany <masatani@fusic.co.jp>
2018-04-11 13:18:27 +0900

    version up

--- fruorepo.rb
+++ fruorepo.rb
@@ -2,8 +2,8 @@
 class Fruorepo < Formula
   desc "We enjoy repository."
   homepage "https://github.com/k-masatany/fruorepo"
-  url "https://github.com/k-masatany/fruorepo/releases/download/v0.2.1/darwin_386_0.2.1.zip"
-  sha256 "9466dbd71d2bf7024319bfe87e5bc2a8d9b7fb5ae6fbdc7f04e519a95e1e94fe"
+  url "https://github.com/k-masatany/fruorepo/releases/download/v0.2.2/darwin_386_0.2.1.zip"
+  sha256 "e3c21cb3e98e6378665362b503533c789578c039278309c56fb1b8a638d56f4d"
   head 'https://github.com/k-masatany/fruorepo.git', :branch => 'master'
 
   def install
